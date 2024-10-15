8c2333aa3d1ea8ec4d23e5e852392c6e54199966
greentfrapp <limsweekiat@gmail.com>
2024-10-15 12:30:04 +0800

    Fix Safari mobile bug

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="h-screen overflow-hidden flex flex-col lg:px-4 lg:py-2">
+  <div class="h-dvh overflow-hidden flex flex-col lg:px-4 lg:py-2">
     <Navbar />
     <router-view></router-view>
   </div>
