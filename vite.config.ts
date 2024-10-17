f028efbd52792d8f6a12370a2c4dc6e4ff9653ca
greentfrapp <limsweekiat@gmail.com>
2024-10-17 13:09:06 +0800

    Update config for runtime compiler

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,8 @@ import path from 'path'
 export default defineConfig({
   resolve:{
     alias:{
-      '@' : path.resolve(__dirname, './src')
+      '@' : path.resolve(__dirname, './src'),
+      vue: 'vue/dist/vue.esm-bundler.js'
     },
   },
   plugins: [vue()],
